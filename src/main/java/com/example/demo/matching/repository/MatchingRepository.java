1958f9d508d5b43036c357ace6afaf10a66447e2
nayonsoso <panda0329@naver.com>
2023-11-02 16:57:06 +0900

    feat: 사용자가 해당 글을 작성했는지 검사하는 함수 생성
    related to: #3

--- src/main/java/com/example/demo/matching/repository/MatchingRepository.java
+++ src/main/java/com/example/demo/matching/repository/MatchingRepository.java
@@ -2,9 +2,12 @@ package com.example.demo.matching.repository;
 
 import com.example.demo.entity.Apply;
 import com.example.demo.entity.Matching;
+import com.example.demo.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MatchingRepository extends JpaRepository<Matching, Long> {
+
+    boolean existsByIdAndSiteUser(Long id, SiteUser siteUser);
 }
--- src/main/java/com/example/demo/matching/service/MatchingServiceImpl.java
+++ src/main/java/com/example/demo/matching/service/MatchingServiceImpl.java
@@ -1,8 +1,13 @@
 package com.example.demo.matching.service;
 
+import com.example.demo.entity.Matching;
+import com.example.demo.entity.SiteUser;
+import com.example.demo.exception.impl.NoPermissionToEditAndDeleteMatching;
+import com.example.demo.exception.impl.UserNotFoundException;
 import com.example.demo.matching.dto.MatchingDetailDto;
 import com.example.demo.matching.dto.MatchingPreviewDto;
 import com.example.demo.matching.repository.MatchingRepository;
+import com.example.demo.repository.SiteUserRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,21 +15,29 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class MatchingServiceImpl implements MatchingService {
+
     private final MatchingRepository matchingRepository;
+    private final SiteUserRepository siteUserRepository;
 
     @Override
     public MatchingDetailDto create(Long userId, MatchingDetailDto matchingDetailDto) {
-
-        return null;
+        SiteUser siteUser = validateUserGivenId(userId);
+        matchingRepository.save(Matching.fromDto(matchingDetailDto, siteUser));
+        return matchingDetailDto;
     }
 
     @Override
-    public MatchingDetailDto update(MatchingDetailDto matchingDetailDto) {
-        return null;
+    public MatchingDetailDto update(Long userId, MatchingDetailDto matchingDetailDto) {
+        SiteUser siteUser = validateUserGivenId(userId);
+        if(!isUserMadeThisMatching(matchingDetailDto.getId(), siteUser)){
+            throw new NoPermissionToEditAndDeleteMatching();
+        }
+        matchingRepository.save(Matching.fromDto(matchingDetailDto, siteUser));
+        return matchingDetailDto;
     }
 
     @Override
-    public void delete(Long matchingId) {
+    public void delete(Long userId, Long matchingId) {
 
     }
 
@@ -36,5 +49,15 @@ public class MatchingServiceImpl implements MatchingService {
     @Override
     public MatchingDetailDto getDetail(Long matchingId) {
         return null;
+    }
+
+    private SiteUser validateUserGivenId(Long userId){
+        SiteUser siteUser = siteUserRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException());
+        return siteUser;
+    }
+
+    private boolean isUserMadeThisMatching(Long matchingId, SiteUser siteUser){
+        return matchingRepository.existsByIdAndSiteUser(matchingId, siteUser);
     }
 }
