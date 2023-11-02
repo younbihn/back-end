0cfa5b2da67642f789cec33f17c91191a40b23f1
nayonsoso <panda0329@naver.com>
2023-11-02 16:57:30 +0900

    feat: 사용자에게 수정, 삭제 권한 없을 때의 예외 생성
    related to: #3

---
+++ src/main/java/com/example/demo/exception/impl/NoPermissionToEditAndDeleteMatching.java
@@ -0,0 +1,17 @@
+package com.example.demo.exception.impl;
+
+import com.example.demo.exception.AbsctractException;
+import org.springframework.http.HttpStatus;
+
+public class NoPermissionToEditAndDeleteMatching extends AbsctractException {
+
+    @Override
+    public int getStatusCode() {
+        return HttpStatus.FORBIDDEN.value();
+    }
+
+    @Override
+    public String getMessage() {
+        return "매칭글을 수정할 권한이 없습니다.";
+    }
+}
