cee68365a1554ec92a1385c4ea094b865b755f47
younbihn <younbihn@naver.com>
2023-11-16 05:24:22 +0900

    feat: 알림 기능 조회 / 삭제 기능 구현 #43

--- src/main/java/com/example/demo/notification/repository/NotificationRepository.java
+++ src/main/java/com/example/demo/notification/repository/NotificationRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.notification.repository;
 
+import com.example.demo.entity.Apply;
 import com.example.demo.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findBySiteUser_Id(Long userId);
+    void deleteByIdAndSiteUser_Id(Long notificationId, Long userId);
 }
