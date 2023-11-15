0a7e802be1b25b628144ffda5f3adcca3d43bfeb
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-11-16 02:27:26 +0900

    refactor: 주소 api에서 우편번호도 받아오도록 수정

--- src/main/java/com/example/demo/matching/dto/RoadAddressDto.java
+++ src/main/java/com/example/demo/matching/dto/RoadAddressDto.java
@@ -1,12 +1,15 @@
 package com.example.demo.matching.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class RoadAddressDto {
     private String roadAddr;
+    private String zipNo;
 }
