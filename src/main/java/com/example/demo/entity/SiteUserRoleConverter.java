38209c3355d330a26c4e602925b62fe65c6f0821
younbihn <younbihn@naver.com>
2023-11-16 20:06:42 +0900

    Refactor: attribute null 에러 수정

--- src/main/java/com/example/demo/entity/SiteUserRoleConverter.java
+++ src/main/java/com/example/demo/entity/SiteUserRoleConverter.java
@@ -13,7 +13,11 @@ public class SiteUserRoleConverter implements AttributeConverter<List<String>, S
     private static final String SPLIT_CHAR = ",";
     @Override
     public String convertToDatabaseColumn(List<String> attribute) {
-        return attribute.stream().map(String::valueOf)
+        if (attribute == null) {
+            return null; // 또는 적절한 기본값 반환
+        }
+        return attribute.stream()
+                .map(String::valueOf)
                 .collect(Collectors.joining(SPLIT_CHAR));
     }
 
